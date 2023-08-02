8c332a6e1937385e1d49bc6cb8901cf3031efa6b
Majomon <mauri.monzon91@gmail.com>
2023-08-01 22:12:41 -0300

    .

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <SafeAreaView style={[styles.container,{backgroundColor:colors[currentTime]}]}>
       <View style={{paddingTop:Platform.OS==="android" && 30}}>
-        <Text style={styles.text}>Pomodoro</Text>
+        <Text style={styles.text}>Pomodosro</Text>
         <Text style={styles.text}>{time}</Text>
         <Header setTime={setTime} currentTime={currentTime} setCurrentTime={setCurrentTime}/>
       </View>
